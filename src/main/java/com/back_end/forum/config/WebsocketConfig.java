c319a9ff96382944b555953fd40f8974d659c10f
Vezem1r <akulovichvanya@gmail.com>
2024-10-19 21:27:00 +0200

    Update security config

--- src/main/java/com/back_end/forum/config/WebsocketConfig.java
+++ src/main/java/com/back_end/forum/config/WebsocketConfig.java
@@ -1,5 +1,6 @@
 package com.back_end.forum.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,6 +21,6 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect");
+        registry.addEndpoint("/connect").setAllowedOrigins(allowedOrigins).withSockJS();
     }
 }
